6068d2e83470cf8af67b8bed0ddaf51d045016d7
CryptoBurgerDev <93739475+CryptoBurgerDev@users.noreply.github.com>
2022-01-26 19:36:58 +0100

    Update CBURGToken.sol

--- CBURGToken.sol
+++ CBURGToken.sol
@@ -1,9 +1,3 @@
-/*
-CRYPTOBURGERS
-Web: https://cryptoburgers.io
-Telegram: https://t.me/cryptoburgersnft
-*/
-
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
@@ -11,7 +5,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract CBURGToken is ERC20Capped, Ownable {
-
     // Antiwhale
     uint256 public startDate;
     uint256 public endDate;
@@ -22,63 +15,18 @@ contract CBURGToken is ERC20Capped, Ownable {
     // Whitelist Addresses
     mapping(address => bool) public whitelistAddresses;
 
-    constructor(address rewardsWallet_)
+    constructor(address rewardsWallet_, address tokensWallet_)
         ERC20("CBURG Token", "CBURG")
         ERC20Capped(100 * 1e6 * 1e18)
     {
-        //!!!! _mint(0x2C260699603E31593fB66E6D6Cd0De5D8148c7c5, 50 * 1e6 * 1e18);
-        _mint(0x62Eb4E8C1629feD1eF5f93f15e69233157B64E1A, 69 * 1e6 * 1e18); // 50M Play to earn
-        //!!!!! _mint(0xeF884E379dB4121E44998875075a232149da3461, 10 * 1e6 * 1e18); // Public Sale
-        _mint(0x3043a13DF5d6D4350279Ea75b561C47D769d18DA, 8 * 1e6 * 1e18); // Liquidity
-        _mint(0x5eA8639E7a7b47184b0f8f136aE280b0186C18CC, 5 * 1e6 * 1e18); // Marketing
-        _mint(0xc1F6e12F3A56766DA16191e03D124Fc197940f2f, 5 * 1e6 * 1e18); // Platform Development
-        _mint(0x3429a4069DD2666bD857B0be74180f64fF67f1aF, 2 * 1e6 * 1e18); // Private Sale
-        _mint(0x7C8449230A7D1857619612F9B0C00169732b524f, 1 * 1e6 * 1e18); // Airdrop
+        _mint(tokensWallet_, 100 * 1e6 * 1e18);
 
         // AntiWhale 2021-01-01 to 2100-01-01
         setAntiWhale(1641028005, 4102477605, 0 * 1e3 * 1e18);
 
         rewardsWallet = rewardsWallet_;
 
-        setPermissions();
-    }
-
-    /*
-    function to add some wallets (Play to earn, team, ...) to a whitelist to allow lock tokens in DxLock when antiwhate is set to 0
-    */
-    function setPermissions() internal {
-        changeWhitelistAddress(
-            0xeBb7e8B99D4C7f0d20370fA4325B0d5506aDd665,
-            true
-        );
-        changeWhitelistAddress(
-            0xeF884E379dB4121E44998875075a232149da3461,
-            true
-        );
-        changeWhitelistAddress(
-            0x3043a13DF5d6D4350279Ea75b561C47D769d18DA,
-            true
-        );
-        changeWhitelistAddress(
-            0x95763B49E631Ce798aA34efB07654618E850d2Ce,
-            true
-        );
-        changeWhitelistAddress(
-            0x5eA8639E7a7b47184b0f8f136aE280b0186C18CC,
-            true
-        );
-        changeWhitelistAddress(
-            0xc1F6e12F3A56766DA16191e03D124Fc197940f2f,
-            true
-        );
-        changeWhitelistAddress(
-            0x3429a4069DD2666bD857B0be74180f64fF67f1aF,
-            true
-        );
-        changeWhitelistAddress(
-            0x7C8449230A7D1857619612F9B0C00169732b524f,
-            true
-        );
+        changeWhitelistAddress(tokensWallet_, true);
     }
 
     /*
